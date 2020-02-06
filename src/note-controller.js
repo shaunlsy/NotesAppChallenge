f4f8be56bfd0efe88800e340698bb3cb1bd444c6
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-02-06 17:19:57 +0000

    display note content

--- src/note-controller.js
+++ src/note-controller.js
@@ -5,15 +5,49 @@
         this.noteList = new NoteList();
         this.noteListView = new NoteListView(this.noteList);
     }
+    NoteController.prototype.addNote = function(note) {
+        this.noteList.storeNotes(note);
+    };
 
+    NoteController.prototype.insertElement = function(id, doc = document) {
+        var element = doc.getElementById(id);
+        element.innerHTML = this.noteListView.returnHTML();
+    };
+
+    makeUrlChangeDisplayNoteContent();
+
+    function makeUrlChangeDisplayNoteContent() {
+        window.addEventListener("hashchange", showNoteForCurrentPage);
+    }
+
+    function showNoteForCurrentPage() {
+        showNote(this.noteList.array[getNoteFromUrl(window.location)].text);
+    }
+
+    function getNoteFromUrl(location) {
+        return location.hash.split("#notes/")[1];
+    }
+
+    function showNote(note) {
+        document.getElementById("app").innerHTML = note;
+    }
     exports.NoteController = NoteController;
 })(this);
 
-NoteController.prototype.addNote = function(note) {
-    this.noteList.storeNotes(note);
-};
+// makeUrlChangeShowAnimalForCurrentPage();
 
-NoteController.prototype.insertElement = function(id, doc = document) {
-    var element = doc.getElementById(id);
-    element.innerHTML = this.noteListView.returnHTML();
-};
+// function makeUrlChangeShowAnimalForCurrentPage() {
+//     window.addEventListener("hashchange", showAnimalForCurrentPage);
+// }
+
+// function showAnimalForCurrentPage() {
+//     showAnimal(getAnimalFromUrl(window.location));
+// }
+
+// function getAnimalFromUrl(location) {
+//     return location.hash.split("#")[1];
+// }
+
+// function showAnimal(animal) {
+//     document.getElementById("animal").innerHTML = animal;
+// }
