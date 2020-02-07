509ea9a5e6ef8c06561fc15e29db472de720fb48
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-02-07 14:44:17 +0000

    TDD note controller test

--- test/note-controller-test.js
+++ test/note-controller-test.js
@@ -21,3 +21,49 @@
         '<ul><li><a href="#notes/0" id="0">I am Shaun</a></li><li><a href="#notes/1" id="1">I am Marcello</a></li></ul>'
     );
 })();
+
+(function testLoadContent() {
+    var note = new Note(
+        "Hello this is Shaun. Nice to meet you. How are you today"
+    );
+    var note2 = new Note(
+        "Goodbye See you next time hopefully it's tommorrow cheers"
+    );
+    var nc = new NoteController();
+    nc.addNote(note);
+    nc.addNote(note2);
+    // mock
+    var document2 = document.implementation.createHTMLDocument("Test");
+    var element = document2.createElement("div");
+    element.id = "app";
+    document2.body.append(element);
+    // mock
+    nc.insertElement("app", document2);
+    var event = new Event("hashchange");
+    makeUrlChangeDisplayNoteContent();
+    document2.getElementById(`${note.id}`).click();
+    assert.isTrue(window.location.href === "http://localhost:8080/#notes/0");
+    assert.isTrue(
+        element.innerHTML ===
+        "<div>Hello this is Shaun. Nice to meet you. How are you today</div>"
+    );
+})();
+
+function makeUrlChangeDisplayNoteContent() {
+    window.addEventListener("hashchange", showNoteForCurrentPage);
+}
+
+function showNoteForCurrentPage() {
+    var snv = new SingleNoteView(
+        nc.noteList.array[getNoteFromUrl(window.location)].text
+    );
+    showNote(snv.render());
+}
+
+function getNoteFromUrl(location) {
+    return location.hash.split("#notes/")[1];
+}
+
+function showNote(note) {
+    document.getElementById("app").innerHTML = note;
+}
